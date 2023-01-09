1ee7d1dc0bfac13cc9593ff39b8dde744e12c504
Cheng Huang <cheng@airmail.im>
2023-01-09 17:29:15 +0800

    add the trigger for diff two dates

--- scripts/date.py
+++ scripts/date.py
@@ -1,5 +1,6 @@
+from os import getenv
 from sys import argv
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
 def get_week_number():
     week_number = date.today().isocalendar()[1]
@@ -19,6 +20,17 @@ def get_work_day():
     end = start + timedelta(days=4)
 
     print(f'{start} ~ {end}')
+
+def diff(*args):
+    format = '%Y-%m-%d'
+
+    date1 = datetime.strptime(getenv('ESPANSO_DATE1'), format)
+    date2 = datetime.strptime(getenv('ESPANSO_DATE2'), format)
+
+    delta = date1 - date2 if date1.timestamp() > date2.timestamp() else date2 - date1
+
+    print(delta.days)
+
 
 if __name__ == '__main__':
     globals()[argv[1]]()
